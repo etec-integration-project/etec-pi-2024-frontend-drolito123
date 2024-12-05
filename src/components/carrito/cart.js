0a5ac6d84c705f6f70c28398946331191371cb37
drolito123 <p.pensado@alumno.etec.um.edu.ar>
2024-12-05 17:11:51 -0300

    .

--- src/components/carrito/cart.js
+++ src/components/carrito/cart.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './Cart.css'; 
+import './cart.css'; 
 
 const Cart = () => {
   const [cart, setCart] = useState([]);
